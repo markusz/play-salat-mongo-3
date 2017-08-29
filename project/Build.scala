5eaf8167adcc71356cb5da88fd1aba88c4177e38
Markus Ziller <mail.markus.ziller@gmail.com>
2017-08-29 17:19:02 +0200

    Adapts Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -5,9 +5,9 @@ object ProjectBuild extends Build {
 
   lazy val buildVersion =  "1.5.9"
 
-  lazy val root = Project(id = "play-plugins-salat", base = file("."), settings = Project.defaultSettings ++ Publish.settings).settings(
-    organization := "net.cloudinsights",
-    description := "MongoDB Salat plugin for PlayFramework 2",
+  lazy val root = Project(id = "play-salat-mongo-3", base = file("."), settings = Project.defaultSettings ++ Publish.settings).settings(
+    organization := "de.markusziller",
+    description := "Forked MongoDB Salat plugin for PlayFramework 2 - Adapted to work with MongoDB 3.0.6, a very common version of MongoDB that caused problem for this plugin ",
     version := buildVersion,
     scalaVersion := "2.11.7",
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
@@ -45,17 +45,17 @@ object Publish {
     publishArtifact in Test := false,
     pomIncludeRepository := { _ => false },
     licenses := Seq("Apache 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
-    homepage := Some(url("https://github.com/cloudinsights/play-salat")),
+    homepage := Some(url("https://github.com/markusz/play-salat-mongo-3")),
     pomExtra := (
       <scm>
-        <url>git://github.com/cloudinsights/play-salat.git</url>
-        <connection>scm:git://github.com/cloudinsights/play-salat.git</connection>
+        <url>git://github.com/markusz/play-salat-mongo-3.git</url>
+        <connection>scm:git://github.com/markusz/play-salat-mongo-3.git</connection>
       </scm>
       <developers>
         <developer>
-          <id>amarjitmult</id>
-          <name>Amarjit Singh</name>
-          <url>http://github.com/cloudinsights</url>
+          <id>markusziller</id>
+          <name>Markus Ziller</name>
+          <url>http://github.com/markusz</url>
         </developer>
       </developers>)
   )
